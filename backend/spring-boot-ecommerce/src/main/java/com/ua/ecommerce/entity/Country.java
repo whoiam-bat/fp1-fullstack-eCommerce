655e91e2e24011fbae72711088563f848fed8502
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-08-12 16:28:21 +0300

    Added entities for country and state

---
+++ backend/spring-boot-ecommerce/src/main/java/com/ua/ecommerce/entity/Country.java
@@ -0,0 +1,34 @@
+package com.ua.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+@ToString
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country", cascade = CascadeType.PERSIST)
+    @ToString.Exclude
+    @JsonIgnore
+    private List<State> states;
+
+}
