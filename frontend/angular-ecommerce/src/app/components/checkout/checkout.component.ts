30ebe63718a1f12d4b70e89a4e652fe02a745956
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-08-12 22:03:32 +0300

    ImplementeBandcamd checkout form totals   Subscribe Checkout component on CartService to get all totals with   BehaviorSubject

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Country } from 'src/app/common/country';
-import { State } from 'src/app/common/state';
-import { PaymentFromService } from 'src/app/services/payment-form.service';
-import { CheckoutFormValidator } from 'src/app/validators/checkout-form-validator';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Country} from 'src/app/common/country';
+import {State} from 'src/app/common/state';
+import {PaymentFromService} from 'src/app/services/payment-form.service';
+import {CheckoutFormValidator} from 'src/app/validators/checkout-form-validator';
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-checkout',
@@ -15,21 +16,23 @@ export class CheckoutComponent implements OnInit {
   checkoutFormGroup: FormGroup
 
   totalPrice: number = 0
-
   totalQuantity: number = 0
 
   creditCardYears: number[] = []
   creditCardMonths: number[] = []
 
   countries: Country[] = []
-
   shippingStates: State[] = []
   billingStates: State[] = []
 
 
-  constructor(private formBuilder: FormBuilder, private paymentFormService: PaymentFromService) {}
+  constructor(private formBuilder: FormBuilder,
+              private paymentFormService: PaymentFromService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+
+    this.reviewCartDetails();
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -46,7 +49,7 @@ export class CheckoutComponent implements OnInit {
         city: new FormControl('', [Validators.required]),
         state: new FormControl('', [Validators.required, CheckoutFormValidator.notOnlyWhiteSpaces]),
         zipCode: new FormControl('', [Validators.required, Validators.minLength(3), CheckoutFormValidator.notOnlyWhiteSpaces])
-      }),      
+      }),
       billingAddress: this.formBuilder.group({
         country: new FormControl('', [Validators.required]),
         street: new FormControl('', [Validators.required, CheckoutFormValidator.notOnlyWhiteSpaces]),
@@ -68,14 +71,14 @@ export class CheckoutComponent implements OnInit {
     console.log("startMonth: " + startMonth)
 
     this.paymentFormService.getCreditCardMonths(startMonth).subscribe(
-      data => { 
+      data => {
         console.log("Credit card months ==> " + JSON.stringify(data))
         this.creditCardMonths = data
       }
     )
 
     this.paymentFormService.getCreditCardYears().subscribe(
-      data => { 
+      data => {
         console.log("Credit card years ==> " + JSON.stringify(data))
         this.creditCardYears = data
       }
@@ -100,14 +103,13 @@ export class CheckoutComponent implements OnInit {
   }
 
   copyShippingToBillingAddress(event: any): void {
-    if(event.target.checked) {
+    if (event.target.checked) {
 
       this.checkoutFormGroup.controls['billingAddress']
-            .setValue(this.checkoutFormGroup.controls['shippingAddress'].value)
+      .setValue(this.checkoutFormGroup.controls['shippingAddress'].value)
 
       this.billingStates = this.shippingStates
-    }
-    else {
+    } else {
       this.checkoutFormGroup.controls['billingAddress'].reset()
 
       this.billingStates = []
@@ -124,7 +126,7 @@ export class CheckoutComponent implements OnInit {
 
     if (selectedYear === currentYear) startMonth = new Date().getMonth() + 1
     else startMonth = 1
-    
+
 
     this.paymentFormService.getCreditCardMonths(startMonth).subscribe(
       data => {
@@ -153,41 +155,84 @@ export class CheckoutComponent implements OnInit {
     )
   }
 
-  get firstName() {return this.checkoutFormGroup.get('customer.firstName');}
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
 
-  get lastName() {return this.checkoutFormGroup.get('customer.lastName');}
-  
-  get email() {return this.checkoutFormGroup.get('customer.email');}
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
 
-
-  get shippingStreet() {return this.checkoutFormGroup.get('shippingAddress.street');}
-
-  get shippingState() {return this.checkoutFormGroup.get('shippingAddress.state');}
-
-  get shippingZipCode() {return this.checkoutFormGroup.get('shippingAddress.zipCode');}
-
-  get shippingCity() {return this.checkoutFormGroup.get('shippingAddress.city');}
-
-  get shippingCountry() {return this.checkoutFormGroup.get('shippingAddress.country');}
+  get email() {
+    return this.checkoutFormGroup.get('customer.email');
+  }
 
 
-  get billingStreet() {return this.checkoutFormGroup.get('billingAddress.street');}
+  get shippingStreet() {
+    return this.checkoutFormGroup.get('shippingAddress.street');
+  }
 
-  get billingState() {return this.checkoutFormGroup.get('billingAddress.state');}
+  get shippingState() {
+    return this.checkoutFormGroup.get('shippingAddress.state');
+  }
 
-  get billingZipCode() {return this.checkoutFormGroup.get('billingAddress.zipCode');}
+  get shippingZipCode() {
+    return this.checkoutFormGroup.get('shippingAddress.zipCode');
+  }
 
-  get billingCity() {return this.checkoutFormGroup.get('billingAddress.city');}
+  get shippingCity() {
+    return this.checkoutFormGroup.get('shippingAddress.city');
+  }
 
-  get billingCountry() {return this.checkoutFormGroup.get('billingAddress.country');}
+  get shippingCountry() {
+    return this.checkoutFormGroup.get('shippingAddress.country');
+  }
 
 
-  get cardType() {return this.checkoutFormGroup.get('creditCard.cardType');}
+  get billingStreet() {
+    return this.checkoutFormGroup.get('billingAddress.street');
+  }
 
-  get nameOnCard() {return this.checkoutFormGroup.get('creditCard.nameOnCard');}
+  get billingState() {
+    return this.checkoutFormGroup.get('billingAddress.state');
+  }
 
-  get cardNumber() {return this.checkoutFormGroup.get('creditCard.cardNumber');}
+  get billingZipCode() {
+    return this.checkoutFormGroup.get('billingAddress.zipCode');
+  }
 
-  get securityCode() {return this.checkoutFormGroup.get('creditCard.securityCode');}
+  get billingCity() {
+    return this.checkoutFormGroup.get('billingAddress.city');
+  }
 
+  get billingCountry() {
+    return this.checkoutFormGroup.get('billingAddress.country');
+  }
+
+
+  get cardType() {
+    return this.checkoutFormGroup.get('creditCard.cardType');
+  }
+
+  get nameOnCard() {
+    return this.checkoutFormGroup.get('creditCard.nameOnCard');
+  }
+
+  get cardNumber() {
+    return this.checkoutFormGroup.get('creditCard.cardNumber');
+  }
+
+  get securityCode() {
+    return this.checkoutFormGroup.get('creditCard.securityCode');
+  }
+
+  private reviewCartDetails() {
+    this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    );
+
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    );
+  }
 }
--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../common/cart-item';
-import { Subject } from 'rxjs';
+import {BehaviorSubject, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +9,10 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { 
+  constructor() {
   }
 
   addToCart(theCartItem: CartItem) {
@@ -26,7 +26,7 @@ export class CartService {
       alreadyExistsInCart = (existingCartItem != undefined);
 
     }
-    
+
     if(alreadyExistsInCart) existingCartItem!.quantity++;
     else this.cartItems.push(theCartItem);
 
@@ -38,7 +38,7 @@ export class CartService {
 
     if(theCartItem.quantity === 0) this.remove(theCartItem);
     else this.computeCartTotals();
-    
+
   }
 
   remove(theCartItem: CartItem) {
