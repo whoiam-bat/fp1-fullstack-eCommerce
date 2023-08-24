6050347df3ac312bb6c0599651546673e9e84403
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-08-24 23:05:17 +0300

    Added repository   For storing customer with his orders

---
+++ backend/spring-boot-ecommerce/src/main/java/com/ua/ecommerce/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.ua.ecommerce.repository;
+
+import com.ua.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
